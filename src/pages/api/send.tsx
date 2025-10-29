6fed2c91b268ea466258be1b2034aadcf7e30087
gabriel miranda <gabrielmfern@outlook.com>
2025-10-29 10:08:11 -0300

    Revert "don't do any error treatment"
    This reverts commit 0f1bd7909a156c94583a832124df5e1a713082a6.

--- src/pages/api/send.tsx
+++ src/pages/api/send.tsx
@@ -10,10 +10,18 @@ export const config = {
 };
 
 export default async function handler() {
-  return resend.emails.send({
+  const response = await resend.emails.send({
     react: <AppleReceiptEmail />,
     subject: 'Apple Receipt',
     from: 'Acme <onboarding@resend.dev>',
     to: ['delivered@resend.dev'],
   });
+
+  if (response.data) {
+    return NextResponse.json({ id: response.data.id }, { status: 200 });
+  }
+
+  if (response.error) {
+    console.error('Something went wrong during email sending', response.error);
+  }
 }
