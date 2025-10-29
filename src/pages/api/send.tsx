0f1bd7909a156c94583a832124df5e1a713082a6
gabriel miranda <gabrielmfern@outlook.com>
2025-10-29 10:07:06 -0300

    don't do any error treatment

--- src/pages/api/send.tsx
+++ src/pages/api/send.tsx
@@ -10,18 +10,10 @@ export const config = {
 };
 
 export default async function handler() {
-  const response = await resend.emails.send({
+  return resend.emails.send({
     react: <AppleReceiptEmail />,
     subject: 'Apple Receipt',
     from: 'Acme <onboarding@resend.dev>',
     to: ['delivered@resend.dev'],
   });
-
-  if (response.data) {
-    return NextResponse.json({ id: response.data.id }, { status: 200 });
-  }
-
-  if (response.error) {
-    console.error('Something went wrong during email sending', response.error);
-  }
 }
